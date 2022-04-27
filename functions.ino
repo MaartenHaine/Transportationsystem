423d1d8e9add60111ea010acb91b4c6ddc6db8ab
pH <104510719+DoctorLego2003@users.noreply.github.com>
2022-04-27 16:53:42 +0200

    Update functions.ino

--- functions.ino
+++ functions.ino
@@ -3,16 +3,26 @@
 Servo servo_translatie;
 Servo servo_rotatie;
 
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
 
 void setup() {
-  servo_rotatie.attach(3);
-  servo_translatie.attach(4);
-  Serial.begin(9600);
+  servo_rotatie.attach();
+  servo_translatie.attach();
+  
+  pinMode(10, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(24, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  float afstandtotbakjes = 0; //nog te bepalen
+  bool rolbandisbijbakjes = false;
+  bool bakjelinks = false; //ligt eraan welk bakje we moeten gebruiken
+  bool bakjerechts = false;
 
 }
 
 void loop() {
   
+    
 }
   
 
@@ -26,4 +36,37 @@ void continuous(int pin, int hoek) {
   servo_rotatie.write(hoek);
 }
 
-void 
+void search(){
+  //IN EEN SNELLE LUS!!!!
+  // Clears the trigPin condition
+   digitalWrite(trigPin, LOW);
+   delayMicroseconds(2);
+   // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+   digitalWrite(trigPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(trigPin, LOW);
+   // Reads the echoPin, returns the sound wave travel time in microseconds
+   duration = pulseIn(echoPin, HIGH);
+   // Calculating the distance
+   distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+   // Displays the distance on the Serial Monitor
+   //Serial.print("Distance: ");
+   //Serial.print(distance);
+   //Serial.println(" cm");
+   bool rolbandisbijbakjes = rolbandisbijbakjes;
+   bool bakjelinks = bakjelinks; 
+   bool bakjerechts = bakjerechts;
+   float afstandtotbakjes = afstandtotbakjes;
+   if (distance == afstandtotbakjes) {
+     bool rolbandisbijbakjes = true;
+   }
+   if (rolbandisbijbakjes == true) {
+     if (bakjelinks = true) {
+       //draai rolband naar links
+     }
+     else if (bakjerechts = true) {
+       //draai rolband naar rechts
+     }
+   
+   } 
+}
